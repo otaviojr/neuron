c20cee5c062d505e95c078112f0523d283f0d177
otaviojr <otavio.ribeiro@gmail.com>
2023-01-04 18:52:08 -0300

    working

--- src/activations/mod.rs
+++ src/activations/mod.rs
@@ -26,3 +26,25 @@ impl Activation for ReLU {
     Tensor::from_data(value.rows(), value.cols(), data)
   }
 }
+
+pub struct Sigmoid {
+
+}
+
+impl Sigmoid {
+  pub fn new() -> Self {
+    Sigmoid {  }
+  }
+}
+
+impl Activation for Sigmoid {
+  fn forward(&self, value: &Tensor) -> Tensor {
+    let data:Vec<f64> = value.data().iter().map(|value| 1.0 / (1.0 + (-value).exp()) ).collect();
+    Tensor::from_data(value.rows(), value.cols(), data)
+  }
+
+  fn backward(&self, value: &Tensor) -> Tensor {
+    let data:Vec<f64> = value.data().iter().map(|value| value * (1.0 - value) ).collect();
+    Tensor::from_data(value.rows(), value.cols(), data)
+  }
+}
