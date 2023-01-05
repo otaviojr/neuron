a351a9b6531091bdbc7cd5addbb2fc00c52b1a9d
otaviojr <otavio.ribeiro@gmail.com>
2023-01-05 09:01:40 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -43,6 +43,7 @@ impl Layer for LinearLayer {
   fn forward(&mut self, input: &Tensor) -> Option<Tensor> {
     println!("Layer weights size = {}x{}", self.weights.rows(), self.weights.cols());
     println!("Bias = {}", self.bias);
+    println!("Weights = {}", self.weights);
     let z1 = self.weights.mul(input).add_value(self.bias);
     let ret = Some(self.activation.forward(&z1));
     self.last_z1 = Some(z1);
