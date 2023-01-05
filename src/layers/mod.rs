d8535cb5c2718d59d72ddc25e7163df00045b825
otaviojr <otavio.ribeiro@gmail.com>
2023-01-04 21:49:17 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -56,6 +56,7 @@ impl Layer for LinearLayer {
   fn backward(&mut self, input: &Tensor) -> Option<Tensor> {
     if let Some(ref z1) = self.last_z1 {
       let dz = input.mul_wise(&self.activation.backward(z1));
+      println!("dz size = {}x{}", dz.rows(), dz.cols());
       if let Some(ref input) = self.last_input {
         let dw = dz.mul(&input.transpose());
         let db = Tensor::from_data(dz.rows(), dz.cols(), dz.data().to_owned());
