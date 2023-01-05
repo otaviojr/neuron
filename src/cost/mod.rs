7b6079a2e7ef6d7b2b94ccaefa5f5a9c2056da9a
otaviojr <otavio.ribeiro@gmail.com>
2023-01-05 09:39:55 -0300

    working

--- src/cost/mod.rs
+++ src/cost/mod.rs
@@ -9,12 +9,12 @@ impl Functions{
     for (prediction, target) in predictions.data().iter().zip(targets.data().iter()) {
         let v = -((target * prediction.ln()) + ((1.0 - target) * (1.0 - prediction).ln()));
         //println!("v={} for {} and {}",v, target, prediction);
-        if v.is_normal() {
+        if !v.is_nan() {
           loss += v;
         }
     }
 
-    -loss / targets.data().len() as f64
+    -loss / targets.cols() as f64
   }
   
   pub fn binary_cross_entropy_loss_derivative(predictions: &Tensor, targets: &Tensor) -> Tensor {  
