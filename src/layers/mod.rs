7a575f60cce1055f7a7e817b6cd995c86ccc6d9b
otaviojr <otavio.ribeiro@gmail.com>
2023-01-06 11:50:38 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -68,16 +68,15 @@ impl Layer for LinearLayer {
         println!("forward_input size = {}x{}", forward_input.rows(), forward_input.cols());
         let dw = dz.mul(&forward_input.transpose()).div_value(input.cols() as f64);
         println!("dw size = {}x{}", dw.rows(), dw.cols());
-        println!("dw = {}", dw);
+        //println!("dw = {}", dw);
         let db = Tensor::from_data(dz.rows(), dz.cols(), dz.data().to_owned());
         println!("db size = {}x{}", db.rows(), db.cols());
-
-        let ret = Some(self.weights.transpose().mul(&dz));
 
         self.weights = self.weights.sub(&dw.mul_value(0.1));
         let dbf = db.sum_row().div_value(input.cols() as f64);
         self.bias = self.bias.sub(&dbf.mul_value(0.1));
 
+        let ret = Some(self.weights.transpose().mul(&dz));
         return ret;
       }
     }
