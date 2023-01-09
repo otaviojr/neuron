5bcef9602a1ac5ffdf9b2b934da874c1440dab6f
otaviojr <otavio.ribeiro@gmail.com>
2023-01-09 12:05:46 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -208,7 +208,7 @@ impl LayerPropagation for ConvLayer {
                 for y1 in 0 .. self.filter_size.0 {
                   for x1 in 0 .. self.filter_size.1 {
                     dw.set(y1,x1,activation.get(y,x) * i.get(y+y1, x+x1));
-                    output.set(y + y1, x + x1, activation.get(y,x) * fc.get(y1,x1) * i.get(y+y1,x+x1));
+                    output.set(y + y1, x + x1, output.get(y + y1, x + x1) + activation.get(y,x) * fc.get(y1,x1) * i.get(y+y1,x+x1));
                   }
                 }
                 db += i.get(y,x);
