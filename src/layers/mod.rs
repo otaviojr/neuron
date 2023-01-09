faf57bf3aeead463f5b735e1010fd0600a3c1ca2
otaviojr <otavio.ribeiro@gmail.com>
2023-01-08 23:25:21 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -183,6 +183,8 @@ impl LayerPropagation for ConvLayer {
 
     let mut final_output = Vec::new();
     
+    println!("CNN Backward Input = {:?}", input);
+
     for ((f,i),b) in self.filters.iter_mut().zip(input.iter()).zip(self.bias.iter()) {
       if let Some(ref forward_input) = self.last_input {
         for (fi,fc) in forward_input.iter().zip(f.iter_mut()) {
