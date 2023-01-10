bb937edfe8cdffe52dae12173863471d2a456a92
otaviojr <otavio.ribeiro@gmail.com>
2023-01-09 23:01:45 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -419,7 +419,7 @@ impl LayerPropagation for PoolingLayer {
                   }
                 }
               }
-              result.set(y+max_y,x+max_x, i.get(input_y, input_x));
+              result.set(y+max_y,x+max_x, i.get(input_y, input_x) * fi.get(y,x));
               input_x += 1;
             }
             input_y += 1;
