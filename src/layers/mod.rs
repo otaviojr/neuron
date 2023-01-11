49d358155ee533b094f98717ac06baa26529c034
otaviojr <otavio.ribeiro@gmail.com>
2023-01-11 17:05:21 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -241,7 +241,7 @@ impl LayerPropagation for ConvLayer {
               for j in (0 .. fi.cols()-self.filter_size.1).step_by(self.config.stride) {
                 for k in 0 .. self.filter_size.0 {
                   for l in 0 .. self.filter_size.1 {
-                    output.set(i/self.config.stride,j/self.config.stride,output.get(i/self.config.stride,j/self.config.stride) + (dz.get(k,l) * fc.get(k,l)));
+                    output.set(i/self.config.stride,j/self.config.stride,output.get(i/self.config.stride,j/self.config.stride) * (dz.get(k,l) * fc.get(k,l)));
                     dw.set(k,l,dw.get(k,l) + fi.get(i+k, j+l) * dz.get(i/self.config.stride,j/self.config.stride));
                   }
                 }
