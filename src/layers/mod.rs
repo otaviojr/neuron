fa571f8ebdaffe2ddf9b5674ecdb8d991eb0f757
otaviojr <otavio.ribeiro@gmail.com>
2023-01-11 00:41:17 -0300

    working

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -224,11 +224,11 @@ impl LayerPropagation for ConvLayer {
               for j in (0 .. fi.cols()-self.filter_size.1).step_by(self.config.stride) {
                 for k in 0 .. self.filter_size.0 {
                   for l in 0 .. self.filter_size.1 {
-                    output.set(i/self.config.stride,j/self.config.stride,output.get(i/self.config.stride,j/self.config.stride) + (inp.get(k,l) * fi.get(i+k,j+l)));
-                    dw.set(k,l,dw.get(k,l) + fi.get(i+k, j+l) * inp.get(i/self.config.stride,j/self.config.stride));
+                    output.set(i/self.config.stride,j/self.config.stride,output.get(i/self.config.stride,j/self.config.stride) + (dz.get(k,l) * fi.get(i+k,j+l)));
+                    dw.set(k,l,dw.get(k,l) + fi.get(i+k, j+l) * dz.get(i/self.config.stride,j/self.config.stride));
                   }
                 }
-                db += inp.get(i/self.config.stride,j/self.config.stride);
+                db += dz.get(i/self.config.stride,j/self.config.stride);
               }
             }
             dw_channel.push(dw)
