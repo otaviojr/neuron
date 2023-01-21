290c657625edf3bd042ef5360acd43f15bdc34e8
otaviojr <otavio.ribeiro@gmail.com>
2023-01-21 00:49:24 -0300

    OpenCL

--- src/math/opencl.rs
+++ src/math/opencl.rs
@@ -398,7 +398,7 @@ impl MatrixMath for MatrixMathOCL {
 
   fn mul_value(&self, a: &Tensor, value: f64) -> Tensor {
     let mult = Tensor::from_data(a.rows, a.cols, vec![value; a.rows * a.cols]);
-    self.div(a, &mult)
+    self.mul(a, &mult)
   }
 
   fn sum_row(&self, a:&Tensor) -> Tensor {
