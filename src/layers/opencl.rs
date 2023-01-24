2f9ce1b194e8d72908a278505501a6a4ef01422f
otaviojr <otavio.ribeiro@gmail.com>
2023-01-24 19:39:25 -0300

    OpenCL

--- src/layers/opencl.rs
+++ src/layers/opencl.rs
@@ -2,7 +2,7 @@ use std::{ptr, time::Instant};
 
 use opencl3::{device::{Device, get_all_devices, CL_DEVICE_TYPE_GPU}, context::Context, command_queue::{CommandQueue, CL_QUEUE_PROFILING_ENABLE}, program::Program, memory::{CL_MEM_READ_ONLY, CL_MEM_WRITE_ONLY, Buffer}, types::{cl_double, CL_BLOCKING, CL_NON_BLOCKING, cl_event}, kernel::{Kernel, ExecuteKernel}};
 
-use crate::{math::{Tensor, opencl::OCL}, Neuron};
+use crate::{math::{Tensor, opencl::OCL, MatrixMathExecutorEnum}, Neuron};
 
 use super::{ConvLayerExecutor, cpu::ConvLayerCPU, ConvLayerConfig};
 
@@ -34,8 +34,6 @@ __kernel void conv(__global double *input, __global double *filter, __global dou
 const KERNEL_MATRIX_CONV_NAME: &str = "conv";
 
 pub struct ConvLayerOCL {
-  device: Option<Device>,
-  context: Option<Context>,
   queue: Option<CommandQueue>,
   program: Option<Program>,
   cpu: ConvLayerCPU
@@ -43,29 +41,20 @@ pub struct ConvLayerOCL {
 
 impl ConvLayerOCL {
   pub fn init() -> Self {
-    let mut device = None;
-    let mut context = None;
     let mut queue = None;
     let mut program = None;
 
-    if let Ok(device_id) = get_all_devices(CL_DEVICE_TYPE_GPU){
-      let d = Device::new(device_id.first().unwrap().clone());
-      Neuron::logger().info(|| format!("OpenCL device (ConvLayerOCL): {}", d.name().unwrap()));
-      if let Ok(c) = Context::from_device(&d) {
-        if let Ok(q) = CommandQueue::create_default(&c, CL_QUEUE_PROFILING_ENABLE) {
-          if let Ok(p) = Program::create_and_build_from_source(&c, PROGRAM_SOURCE, "") {
-            device= Some(d);
-            context = Some(c);
-            queue = Some(q);
-            program = Some(p);
-          }
+    let executor = Neuron::matrix();
+    if let MatrixMathExecutorEnum::OCL(ref matrix_ocl) = **executor {
+      if let Ok(q) = CommandQueue::create_default(&matrix_ocl.get_ocl_context().unwrap(), CL_QUEUE_PROFILING_ENABLE) {
+        if let Ok(p) = Program::create_and_build_from_source(&matrix_ocl.get_ocl_context().unwrap(), PROGRAM_SOURCE, "") {
+          queue = Some(q);
+          program = Some(p);
         }
       }
     }
-
+    
     ConvLayerOCL {
-      device,
-      context,
       queue,
       program,
       cpu: ConvLayerCPU::init()
@@ -75,47 +64,51 @@ impl ConvLayerOCL {
 
 impl ConvLayerOCL{
   fn do_conv(&self, input: &Box<Tensor>, filter: &Tensor, bias: &f64, result: &mut Tensor, config: &ConvLayerConfig) {
-    if let Some(ref context) = self.context {
-      if let Some(ref queue) = self.queue {
-        if let Some(ref program) = self.program {
-          let input_ocl = input.get_ocl_buffer();
-          let filter_ocl = filter.get_ocl_buffer();
 
-          let input_buffer = input_ocl.lock().unwrap();
-          let filter_buffer = filter_ocl.lock().unwrap();
-          let result_buffer = unsafe {
-            Buffer::<cl_double>::create(context, CL_MEM_WRITE_ONLY, result.data().len(), ptr::null_mut()).unwrap()
-          };  
+    let executor = Neuron::matrix();
+    if let MatrixMathExecutorEnum::OCL(ref matrix_ocl) = **executor {
+      if let Some(context) = matrix_ocl.get_ocl_context() {
+        if let Some(ref queue) = self.queue {
+          if let Some(ref program) = self.program {
+            let input_ocl = input.get_ocl_buffer();
+            let filter_ocl = filter.get_ocl_buffer();
 
-          let kernel = Kernel::create(&program, KERNEL_MATRIX_CONV_NAME).unwrap();
+            let input_buffer = input_ocl.lock().unwrap();
+            let filter_buffer = filter_ocl.lock().unwrap();
+            let result_buffer = unsafe {
+              Buffer::<cl_double>::create(context, CL_MEM_WRITE_ONLY, result.data().len(), ptr::null_mut()).unwrap()
+            };  
 
-          let kernel_event = unsafe {
-            ExecuteKernel::new(&kernel)
-                .set_arg(&*input_buffer)
-                .set_arg(&*filter_buffer)
-                .set_arg(&result_buffer)
-                .set_arg(&bias)
-                .set_arg(&(input.cols() as i32))
-                .set_arg(&(input.rows() as i32))
-                .set_arg(&(filter.cols() as i32))
-                .set_arg(&(filter.rows() as i32))
-                .set_arg(&(result.cols() as i32))
-                .set_arg(&(result.rows() as i32))
-                .set_arg(&(config.stride as i32))
-                .set_arg(&(config.padding as i32))
-                .set_global_work_size(result.data().len())
-                .enqueue_nd_range(&queue).unwrap()
-          };
+            let kernel = Kernel::create(&program, KERNEL_MATRIX_CONV_NAME).unwrap();
 
-          let mut events: Vec<cl_event> = Vec::default();
-          events.push(kernel_event.get());
-          
-          let ret = unsafe { queue.enqueue_read_buffer(&result_buffer, CL_NON_BLOCKING, 0, &mut result.mut_data(), &events).unwrap() };
-          let error = ret.wait();
+            let kernel_event = unsafe {
+              ExecuteKernel::new(&kernel)
+                  .set_arg(&*input_buffer)
+                  .set_arg(&*filter_buffer)
+                  .set_arg(&result_buffer)
+                  .set_arg(&bias)
+                  .set_arg(&(input.cols() as i32))
+                  .set_arg(&(input.rows() as i32))
+                  .set_arg(&(filter.cols() as i32))
+                  .set_arg(&(filter.rows() as i32))
+                  .set_arg(&(result.cols() as i32))
+                  .set_arg(&(result.rows() as i32))
+                  .set_arg(&(config.stride as i32))
+                  .set_arg(&(config.padding as i32))
+                  .set_global_work_size(result.data().len())
+                  .enqueue_nd_range(&queue).unwrap()
+            };
 
-          if let Err(error) = error {
-            Neuron::logger().error(|| format!("OpenCL Error: {:?}", error));
-            std::process::exit(0);
+            let mut events: Vec<cl_event> = Vec::default();
+            events.push(kernel_event.get());
+            
+            let ret = unsafe { queue.enqueue_read_buffer(&result_buffer, CL_NON_BLOCKING, 0, &mut result.mut_data(), &events).unwrap() };
+            let error = ret.wait();
+
+            if let Err(error) = error {
+              Neuron::logger().error(|| format!("OpenCL Error: {:?}", error));
+              std::process::exit(0);
+            }
           }
         }
       }
