cded59219be61fcd9ffb673dfa202e5302c67dca
otaviojr <otavio.ribeiro@gmail.com>
2023-01-27 13:32:42 -0300

    OpenCL

--- src/math/opencl.rs
+++ src/math/opencl.rs
@@ -13,7 +13,7 @@ __kernel void add(__global float *a, __global float *b, __global float *c, int w
 __kernel void add_bulk(__global float *a, __global float *b, int blocks, int len, int width, int height) {
   int gid = get_global_id(0);
 
-  forint i = 0; i < blocks; i++)
+  for(int i = 0; i < blocks; i++)
     for(int j = 0; i < len; i++)
       b[gid + i * width * height ] += a[gid + i * j * width * height];
 }
